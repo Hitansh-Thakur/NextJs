b28c687e9dbe6bd80a5c3a323cb2dde32e7a1c9a
Hitansh Thakur <thakurhitansh4325@gmail.com>
2022-05-23 11:48:34 +0530

    initial

---
+++ components/Task.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { test } from '../pages/todo'
+import { titleInput} from '../pages/todo'
+
+// console.log("task")
+
+const Task = () => {
+  return (<>
+    {/* <div>{titleInput}</div> */}
+    {/* console.log(test); */}
+    </>
+  )
+}
+
+export default Task
